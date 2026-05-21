51f802f4958091621dc174b06dfbf0dab082d4f5
ANIXODIA <renly271@gmail.com>
2026-05-21 12:56:43 +0700

    Update loginmenu.php

--- loginmenu.php
+++ loginmenu.php
@@ -14,8 +14,87 @@ if(mysqli_num_rows($result) > 0){
         echo "<center><b><font color='red' size='6'>WRONG PASSWORD! YOU DIED!</font></b></center>";
         }
 }
+
+if(isset($_POST["registerBtn"])){ 
+    $usr = $_POST["username"];
+    $pw = $_POST["password"];
+  $sql2 = "INSERT INTO players (username, password) VALUES ('$usr', '$pw')";
+    if(mysqli_query($conn, $sql2)){
+        echo "<center><b><font color='lime' size='6'>ACCOUNT CRAFTED! You can join the server now!</font></b></center>";
+    } else {
+        echo "<center><b><font color='red' size='6'>SERVER LAG! Could not craft account! (Maybe name is taken?)</font></b></center>";
+    }
 ?>
 <html>
+    <head>
+    <title>777 Mineslots 777</title>
+    <style>
+        body {
+            background-color: #5C4033; 
+            color: white;
+            font-family: "Comic Sans MS", cursive, sans-serif; /
+        }
+        .stone-box {
+            background-color: #808080; 
+            border: 8px solid #333333;
+            width: 380px;
+            padding: 20px;
+        }
+        .grass-btn {
+            background-color: #4CBB17; 
+            color: white;
+            font-size: 22px;
+            border: 4px solid #228B22;
+            font-weight: bold;
+            cursor: pointer;
+        }
+        .wood-btn {
+            background-color: #8B5A2B; 
+            color: white;
+            font-size: 20px;
+            border: 4px solid #5C4033;
+            font-weight: bold;
+            margin-top: 10px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+
+<center>
+    <br><br><br>
+    <h1><font color="#4CBB17">~~~ Welcome to Mineslots777 Server ~~~</font></h1>
+    <h3><font color="cyan">(spend wisely pls)</font></h3>
+    <br>
+
+    <div class="stone-box">
+        <center>
+            <form action="" method="POST">
+                <u><b>Player Name:</b></u><br>
+        
+                <input type="text" name="username" style="width: 200px; background-color: #cccccc;"><br><br>
+
+                <u><b>Secret Password:</b></u><br>
+                <input type="password" name="password" style="width: 200px; background-color: #cccccc;"><br><br>
+
+            
+                <input type="submit" name="loginBtn" value=" JOIN SERVER " class="grass-btn">
+                <br>
+              
+                <input type="submit" name="registerBtn" value=" CRAFT NEW ACCOUNT " class="wood-btn">
+            </form>
+        </center>
+    </div>
+
+    <br><br>
+    <marquee width="60%" scrollamount="20" bgcolor="black">
+        <font color="yellow" size="5">SERVER IS ONLINE (Ver 1.0.0) PLS DONATE FOR DIAMOND! </font>
+    </marquee>
+
+</center>
+
+</body>
+</html>
 
 
 
