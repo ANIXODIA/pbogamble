63d6e65102bcdcfd176f5eb903e22290686f9495
ANIXODIA <renly271@gmail.com>
2026-05-21 09:32:32 +0700

    Update loginmenu.php

--- loginmenu.php
+++ loginmenu.php
@@ -1,3 +1,31 @@
-<?php>
+<?php
 session start():
 require ONCE ('database.php')
+if(isset($_POST["loginBtn"])){
+    $usr = $_POST["username"];
+    $pw = $_POST["password"];
+$sql = "SELECT * FROM players WHERE username = '$usr' AND password = '$pw'";
+$result = mysqli_query($conn, $sql);
+if(mysqli_num_rows($result) > 0){
+        $_SESSION["user"] = $usr;
+        echo "<script>alert('Login Epic Win! Welcome to the server!');</script>";
+    } else {
+        // creeper explosion error message
+        echo "<center><b><font color='red' size='6'>WRONG PASSWORD! YOU DIED!</font></b></center>";
+        }
+}
+?>
+<html>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
