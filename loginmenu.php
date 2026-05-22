4b58cdd8a5546e77f28396f741588ef9ae121997
ANIXODIA <renly271@gmail.com>
2026-05-22 08:20:25 +0700

    Update loginmenu.php

--- loginmenu.php
+++ loginmenu.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-$conn = mysqli_connect("localhost", "root", "", "my_game_db");
+session_start(); 
+$conn = mysqli_connect("localhost", "root", "", "my_game_db"); 
 
 if(isset($_POST["loginBtn"])){
     $usr = $_POST["username"];
@@ -10,29 +10,17 @@ if(isset($_POST["loginBtn"])){
     
     if(mysqli_num_rows($result) > 0){
         $_SESSION["user"] = $usr;
-        header("Location: loai.php?user=" . urlencode($usr));
+        Send them to the game
+        header("Location: main.php?user=" . urlencode($usr));
         exit();
     } else {
-        echo "<center><b><font color='red' size='6'>WRONG PASSWORD!</font></b></center>";
+        echo "<center><b><font color='red' size='6'>WRONG PASSWORD OR USERNAME!</font></b></center>";
     }
 }
-
-if(isset($_POST["registerBtn"])){ 
-    $usr = $_POST["username"];
-    $pw = $_POST["password"];
-    // Assuming you have a 'money' column in the players table, we start them with 1000
-    $sql2 = "INSERT INTO players (username, password, money) VALUES ('$usr', '$pw', 1000)";
-    
-    if(mysqli_query($conn, $sql2)){
-        echo "<center><b><font color='lime' size='6'>ACCOUNT CRAFTED! You can join the server now!</font></b></center>";
-    } else {
-        echo "<center><b><font color='red' size='6'>SERVER LAG! Could not craft account! (Maybe name is taken?)</font></b></center>";
-    }
-} // Fixed the missing closing bracket here!
 ?>
 <html>
 <head>
-    <title>777 Mineslots 777</title>
+    <title>777 Mineslots 777 - Login</title>
     <style>
         body {
             background-color: #5C4033; 
@@ -76,17 +64,16 @@ if(isset($_POST["registerBtn"])){
         <center>
             <form action="" method="POST">
                 <u><b>Player Name:</b></u><br>
-        
                 <input type="text" name="username" style="width: 200px; background-color: #cccccc;"><br><br>
 
                 <u><b>Secret Password:</b></u><br>
                 <input type="password" name="password" style="width: 200px; background-color: #cccccc;"><br><br>
 
                 <input type="submit" name="loginBtn" value=" JOIN SERVER " class="grass-btn">
-                <br>
-              
-                <input type="submit" name="registerBtn" value=" CRAFT NEW ACCOUNT " class="wood-btn">
             </form>
+            
+            <br>
+            <button onclick="window.location.href='register.php'" class="wood-btn"> CRAFT NEW ACCOUNT </button>
         </center>
     </div>
 
